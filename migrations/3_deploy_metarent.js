f043ce839bf3046b57abdd0464b3c12fd37976d9
Shawn Tian <hightian@gmail.com>
2021-12-27 18:53:30 +0800

    add missing account parameter while deploy

--- migrations/3_deploy_metarent.js
+++ migrations/3_deploy_metarent.js
@@ -1,7 +1,7 @@
 const Metarent = artifacts.require("Metarent");
 
 module.exports = async (deployer, network, accounts) => {
-  await deployer.deploy(Metarent);
+  await deployer.deploy(Metarent, accounts[0]);
 
   const metarent = await Metarent.deployed();
 };
